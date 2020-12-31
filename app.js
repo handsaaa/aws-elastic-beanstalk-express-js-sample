b188e0b4ff284987db38d8f959df3c1d9848ae46
Ubuntu <ubuntu@ip-172-31-29-212.ec2.internal>
2020-12-31 08:05:49 +0000

    Change Message Pipeline Test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hi Iam Islam and Iam SRE Engineer'));
+app.get('/', (req, res) => res.send('Hi Iam Islam and Iam SRE Engineer, This is a first Full pipeline test'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
